3790d4d075d095369b7229058ba49424b878687f
CrazyGG <eninja@outlook.com>
2012-08-12 23:52:10 +0800

    add redbank/src/redbank/ioc/parameter.h

---
+++ redbank/src/redbank/ioc/parameter.h
@@ -0,0 +1,22 @@
+#ifndef RED_BANK_IOC_PARAMETER_H
+#define RED_BANK_IOC_PARAMETER_H
+
+/**
+\file parameter.h
+*/
+
+
+namespace redbank {
+
+namespace ioc{
+
+class Parameter{
+public:
+    virtual ~Parameter()
+    {}
+public:
+
+};
+}
+}
+#endif // RED_BANK_IOC_PARAMETER_H
